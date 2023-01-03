fc7e3392a630100f5a5f0bb0ee3544837dbdd8bc
matheus <matheusg_souza@outlook.com>
2023-01-03 10:32:54 -0300

    pjct[03]:Finishing application

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
-import { SendNotification } from '../../application/use-cases/send-notification';
+import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+    CountRecipientNotifications,
+  ],
 })
 export class HttpModule {}
